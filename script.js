69a7db485d80afa20a3efbe4b4110080ec6925bf
hashimxkhan <khan521@purdue.edu>
2024-06-01 13:16:32 +0500

    Added RGB Generator Added an extra feature that allows user to fill up squares with randomly generated colors

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const container = document.querySelector(".container")
-function squares(num) {
-    let dimension = (512 / num) - 2
-    let k = 0
+hovColor = "blue"
+let val = 16
+function squares(num, hovColor) {
+    let dimension = (550 / num) - 2
     for (let i = 1; i <= (num ** 2 + num + 1); i++) {
         const square = document.createElement("div")
         container.appendChild(square)
@@ -14,24 +15,56 @@ function squares(num) {
         }
         container.appendChild(square) 
         square.addEventListener("mouseenter", function() {
-            square.style.backgroundColor = "blue"
+            square.style.backgroundColor = hovColor
         })
     }
 }
-squares(16)
+squares(val, hovColor) 
 
 const buttons = document.querySelector(".buttons")
 const size = document.createElement("button")
 size.textContent = "Resize Grid"
 buttons.appendChild(size)
+const rgb = document.createElement("button")
+rgb.className = "btn"
+rgb.textContent = "RGB Generator"
+buttons.appendChild(rgb)
+size.className = "btn"
 
-size.addEventListener("click", function() {
-  /* while(true) { */
-        let val =  prompt("Enter your preferred grid size (10-100)")
-        val = parseInt(val)
-       /* if (val >= 10 && val <= 100 && !isNaN(val)) {
-            break
-        } 
-    } */
-    squares(val)
+size.addEventListener("click", function() { 
+     val =  prompt("Enter your preferred grid size (10-100)")
+     val = parseInt(val)
+     container.innerHTML = ''
+     squares(val, hovColor)
 })
+
+rgb.addEventListener("click", function() {
+    container.innerHTML = ''
+    rgbSquares(val)
+})
+
+function randomColor() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    return "rgb(" + r + ", " + g + ", " + b + ")"
+}
+
+function rgbSquares(num) {
+    let dimension = (550 / num) - 2 
+    for (let i = 1; i <= (num ** 2 + num + 1); i++) {
+        const square = document.createElement("div")
+        container.appendChild(square)
+        if ((i + 1) % num === num - 1) {
+            square.className = "special"
+        } else {
+            square.className = "square"
+            square.style.height = dimension + "px"
+            square.style.width = dimension + "px"
+        }
+        container.appendChild(square) 
+        square.addEventListener("mouseenter", function() {
+            square.style.backgroundColor = randomColor()
+        })
+    }
+}
